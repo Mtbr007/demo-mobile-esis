0a8e97820ce2a49e6d164f061b522641d9f8c8b6
Jeremie Zozo Matabaro <19zm526@esisalama.org>
2022-07-05 17:30:56 +0200

    de

--- app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
+++ app/src/main/java/org/esissalama/mobile/demo/MainActivity.java
@@ -3,12 +3,55 @@ package org.esissalama.mobile.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView diceImage;
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        diceImage =findViewById(R.id.imageView2);
+
+        diceImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateDice();
+            }
+        });
+    }
+    private void rotateDice(){
+        int i =random.nextInt(5)+1;
+        switch (i){
+            case 1:
+                diceImage.setImageResource(R.drawable.dice_1);
+                break;
+            case 2:
+                diceImage.setImageResource(R.drawable.dice_2);
+                break;
+
+            case 3:
+                diceImage.setImageResource(R.drawable.dice_3);
+                break;
+
+            case 4:
+                diceImage.setImageResource(R.drawable.dice_4);
+                break;
+            case 5:
+                diceImage.setImageResource(R.drawable.dice_5);
+                break;
+            case 6:
+                diceImage.setImageResource(R.drawable.dice_6);
+                break;
+
+        }
     }
 }
